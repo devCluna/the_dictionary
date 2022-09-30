fcdcab093a4c5e084fbd3516268d20355e18b17c
Cristhian <jobs.cluna@gmail.com>
2022-09-30 18:55:41 -0500

    feat: add searForm comp

---
+++ src/views/components/SearchForm.jsx
@@ -0,0 +1,56 @@
+import { Field, Form, Formik } from 'formik'
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { getWord } from '../../redux/states/dictionary'
+import './SearchForm.css'
+
+const SearchForm = () => {
+    const dispatch = useDispatch()
+
+    return (
+        <div className='search-form'>
+            <Formik
+                initialValues={{
+                    search: ""
+                }}
+                validate={(values) => {
+
+                    let errors = {}
+
+                    if (!values.search) {
+                        errors.search = "Please type a search"
+                    } else if (/^[0-9]+$/.test(values.search)) {
+                        errors.search = "Numbers are not allowed"
+                    }
+
+                    return errors
+                }}
+                onSubmit={(values) => {
+                    dispatch(getWord(values.search))
+                }}
+            >
+                {({ errors, values, handleSubmit, handleChange, handleBlur }) => (
+                    <Form onSubmit={handleSubmit} className="form bg-indigo-500">
+
+                        <Field
+                            className=""
+                            type="text"
+                            id="search"
+                            name="search"
+                            value={values.search}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            placeholder='Type your word'
+                        />
+
+                        <button type='submit'>Search</button>
+                        {errors.search && <p className='errors'>{errors.search}</p>}
+                    </Form>)
+                }
+            </Formik>
+        </div>
+
+    )
+}
+
+export default SearchForm
