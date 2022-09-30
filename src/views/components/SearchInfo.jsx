59522a3d5f9962ed8225f6824228e6dcbb40543e
Cristhian <jobs.cluna@gmail.com>
2022-09-30 18:55:58 -0500

    feat: add search info comp

---
+++ src/views/components/SearchInfo.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useSelector} from 'react-redux'
+import uniqid from 'uniqid'
+import './SearchInfo.css'
+
+const SearchInfo = () => {
+    //Format Info
+    const  {wordSearch:{word},
+            wordSearch:{phonetic}, 
+            wordSearch:{phonetics},
+            wordSearch:{meanings},  
+            loading
+        } = useSelector(state => state.dictionary)
+
+    return (
+        <div className='search-info'>
+            {word && <p>word: <span>{word}</span></p>}
+            {phonetic && <p>phonetic: <span>{phonetic}</span></p>}
+            
+            {/* {phonetics?.map(phonetic => (
+                
+                <audio key={uniqid()} src={phonetic.audio} controls/>
+            ))} */}
+
+            {meanings?.map(meaning => (
+                <div key={uniqid()}>
+                    <p>Parth of Speech: {meaning.partOfSpeech}</p>
+                    <p>Definitions:</p>
+                    {meaning?.definitions.map(mean =>(
+                        <p key={uniqid()}>def {meaning.definitions.indexOf(mean)+1}: <span>{mean.definition}</span></p>
+                    ))}
+                </div>
+            ))}
+        </div>
+    )
+  }
+
+
+export default SearchInfo
